a07f5b842b7c3a8af3d6006125e8b36cb0fda0c9
Samuel Githinji <samuelndewa2018@gmail.com>
2025-05-26 21:36:48 +0300

    interger

---
+++ 5-to_integer.js
@@ -0,0 +1,9 @@
+const arg = process.argv[2];
+
+const num = Number(arg);
+
+if (!isNaN(num) && Number.isInteger(num)) {
+  console.log(`My number: ${num}`);
+} else {
+  console.log("Not a number");
+}
